c657ab798fa7a49685ac166b4785f2e7312544f1
KillyCurri <killybennie414@gmail.com>
2026-09-01 13:56:24 +0300

    4

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : ''
 
 export default defineConfig(({ mode }) => {
-  const base = process.env.VITE_PUBLIC_PATH || (repoName ? `/${repoName}/` : '/')
+  const base = process.env.VITE_PUBLIC_PATH || (repoName ? `/${repoName}/` : './')
   return {
     base,
     plugins: [react()],
